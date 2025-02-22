53c9fa362cdde799b3eedfbe30feb3883fb78553
Rovastus11 <rovastus1+1@gmail.com>
2025-02-22 18:28:35 +0100

    Add debug mode

--- AppConfig.java
+++ AppConfig.java
@@ -2,6 +2,12 @@ public class AppConfig {
     public static void main(String[] args) {
         System.out.println("Application started.");
         System.out.println("Version: 1.0");
-        System.out.println("Environment: Production");
+        System.out.println("Environment: Development");
+
+        enableDebugMode();
+    }
+
+    private static void enableDebugMode() {
+        System.out.println("Debug mode enabled.");
     }
 }
