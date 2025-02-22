ec5a99f86fe3a36d5736f54fbc707784fe9568f6
Rovastus11 <rovastus1+1@gmail.com>
2025-02-22 18:27:48 +0100

    Create AppConfig.java

---
+++ AppConfig.java
@@ -0,0 +1,7 @@
+public class AppConfig {
+    public static void main(String[] args) {
+        System.out.println("Application started.");
+        System.out.println("Version: 1.0");
+        System.out.println("Environment: Production");
+    }
+}
