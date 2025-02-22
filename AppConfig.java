927dd49e3a89d0a79762bca1fe425976aac606fe
Rovastus11 <rovastus1+1@gmail.com>
2025-02-22 18:29:05 +0100

    Add security check

--- AppConfig.java
+++ AppConfig.java
@@ -1,7 +1,13 @@
 public class AppConfig {
     public static void main(String[] args) {
         System.out.println("Application started.");
-        System.out.println("Version: 1.0");
+        System.out.println("Version: 1.1");
         System.out.println("Environment: Production");
+
+        checkSecurity();
+    }
+
+    private static void checkSecurity() {
+        System.out.println("Security checks passed.");
     }
 }
